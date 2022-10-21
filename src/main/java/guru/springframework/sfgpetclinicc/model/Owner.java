e0a714442d2cd49dff0534b3563700b28c2f5801
Julian Sech <info@qsamax.org>
2022-10-21 13:49:52 +0200

    adding POJOs. Closses #2.

---
+++ src/main/java/guru/springframework/sfgpetclinicc/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinicc.model;/* 21/10/2022" "12:44" "Julian */
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinicc/model/PetType.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinicc.model;/* 21/10/2022" "12:48" "Julian */
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
