2c378cee53afe82403363d769c1eafe3ccb5508a
Julian Sech <info@qsamax.org>
2022-10-21 11:08:19 +0200

    Initial Project Add

---
+++ src/test/java/guru/springframework/sfgpetclinicc/SfgPetCliniccApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinicc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetCliniccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
