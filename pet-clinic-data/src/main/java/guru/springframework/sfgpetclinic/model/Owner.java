8518fe0219318f49f206a1d3b427c52060ea6232
Julian Sech <info@qsamax.org>
2022-11-02 15:36:25 +0100

    added visits, updated owner with pet relationship. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,17 @@
 package guru.springframework.sfgpetclinic.model;/* 21/10/2022" "12:44" "Julian */
 
+
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
