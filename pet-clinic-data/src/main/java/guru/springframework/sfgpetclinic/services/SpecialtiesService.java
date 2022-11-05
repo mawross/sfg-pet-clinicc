727da3dae0d9cde9d91f433155def51f69800eb3
Julian Sech <info@qsamax.org>
2022-11-05 09:07:18 +0100

    Added Specialties Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;/* 04/11/2022" "18:21" "Julian */
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
