716a1f490d33b670d482c66b393cc5871151aa25
Julian Sech <info@qsamax.org>
2022-11-11 20:02:45 +0100

    Created visit entity. Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package guru.springframework.sfgpetclinic.model;/* 21/10/2022" "12:45" "Julian */
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -14,7 +13,7 @@ public class Pet extends BaseEntity {
     private String name;
 
     @ManyToOne
-    @Column(name = "type_id")
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
     @ManyToOne
@@ -23,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -56,4 +58,11 @@ public class Pet extends BaseEntity {
         this.birthDate = birthDate;
     }
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 }
